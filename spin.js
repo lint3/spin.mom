97652007d8532b6713b9d5ecf8426c27344c86b8
Jacob <git@cyptem.com>
2023-05-28 13:07:18 -0600

    Crop image

--- spin.js
+++ spin.js
@@ -1,10 +1,10 @@
 let element = document.getElementById('sprite');
 let sprite = new AnimateSprite(element, {
-  width: 960, /* required */
+  width: 480, /* required */
   height: 540, /* required */
   frames: 35, /* required */
   cols: 35,
-  fps: 60,
+  fps: 12,
   loop: true,
   draggable: true
 });
